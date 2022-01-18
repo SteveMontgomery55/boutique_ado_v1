11737bd84840e9eb7edd36ab821832b186e5fbd4
SteveMontgomery55 <stevemontgomeryuk@yahoo.co.uk>
2022-01-18 11:50:47 +0000

    updated django to fix errors

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -47,8 +48,6 @@ INSTALLED_APPS = [
     'bag',
     'checkout',
     'profiles',
-
-    # Other
     'crispy_forms',
 ]
 
